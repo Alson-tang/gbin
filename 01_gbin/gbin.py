d00e31c47ea2deb6605e4b3eea312c42ddd41978
Tang Xiang <tangxiang@espressif.com>
2021-12-20 21:10:57 +0800

    feat: Optimize script

--- 01_gbin/gbin.py
+++ 01_gbin/gbin.py
@@ -1,149 +1,69 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 alson <tx_danyang@163.com>
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE', which is part of this source code package.
+
 import os
 import sys
+import argparse
 import struct
 import random
-from enum import Enum
 
-CMD_HELP_PARAM_NUMS = 2
-CMD_EXEC_PARAM_NUMS = 4
+def paras_analysis():
+    parser = argparse.ArgumentParser(description='generate a bin file of the specified size')
+    parser.add_argument('-of', default='random.bin', help='output file name')
+    parser.add_argument('-unit', default='B', help='unit, can only be B or KB or MB')
+    parser.add_argument('-count', default=1, help='copy only N input blocks')
+    args = parser.parse_args()
+    return args
 
-class CMD(Enum):
-    CMD_INVALID = 0
-    CMD_HELP = 1
-    CMD_EXEC = 2
+def args_valid_check(file_name, unit, count):
+    if unit != 'B' and unit != 'KB' and unit != 'MB':
+        print(f"-unit parameter invalid")
+        return False
 
-def help_info_display(help_param_str):
-    help_param_str = str(help_param_str)
+    if not count.isdigit():
+        print(f"-count parameter invalid")
+        return False
 
-    if help_param_str != '-help' and help_param_str != '-h':
-        print('unknown option:' + help_param_str)
-    else:
-        print('usage: gbin [-of] [-unit] [-count]')
-        print('generate a bin file of the specified size')
-        print('')
-        print('-of=FILE        output file name')
-        print('-unit=UNIT      unit, can only be B or K or M')
-        print('-count=COUNT    copy only N input blocks')
-        print('')
-        print('example: the generated file name is test.bin and the bin file size is 1024 bytes')
-        print('python3 gbin.py -of=test.bin -unit=1B -count=1024')
+    return True
 
-def file_create(cmd_info):
-    file = open('./' + cmd_info['file_name'], 'wb')
-    file.close()
+def out_file_check(file_name):
+    return os.path.exists(file_name)
 
-def file_exists_check(file_name):
-    return os.path.exists('./' + file_name)
+def out_file_size(unit, count):
+    unit_map = { 'B' : 1, 'KB' : 1024, 'MB' : 1024 * 1024}
+    return unit_map.get(unit, 0) * int(count)
 
-def file_write(cmd_info):
-    file = open('./' + cmd_info['file_name'], 'wb')
+def out_file_generate(file_name, file_size):
+    file = open(file_name, 'wb')
 
-    i = 0
-    len = cmd_info['unit'] * cmd_info['count']
-
-    while(i < len):
+    for i in range(0, file_size):
         random_byte = struct.pack('B', random.randint(0, 255))
         file.write(random_byte)
-        i = i + 1
 
     file.close()
 
-def count_get(count_param_str, cmd_info):
-    count_param_str = str(count_param_str)
+    return
 
-    cmd_str = count_param_str[:count_param_str.find('=')]
-    if cmd_str != '-count':
-        print('the third parameter is -count=')
-        return False
-
-    count_str = count_param_str[count_param_str.find('=') + 1:]
-    count = int(count_str)
-
-    cmd_info['count'] = count
-
-    return True
-
-def unit_get(unit_param_str, cmd_info):
-    unit_param_str = str(unit_param_str)
-
-    cmd_str = unit_param_str[:unit_param_str.find('=')]
-    if cmd_str != '-unit':
-        print('the second parameter is -unit=')
-        return False
-
-    unit_char = unit_param_str[-1]
-    unit_capacity = 0
-    if unit_char == 'b' or unit_char == 'B':
-        unit_capacity = 1
-    elif unit_char == 'k' or unit_char == 'K':
-        unit_capacity = 1024
-    elif unit_char == 'm' or unit_char == 'M':
-        unit_capacity = 1024 * 1024
-    else:
-        print('capacity unit error, the unit of capacity can only be B or K or M')
-        return False
-
-    unit_str = unit_param_str[unit_param_str.find('=') + 1:-1]
-    unit_num = int(unit_str)
-    unit = unit_num * unit_capacity
-
-    cmd_info['unit'] = unit
-
-    return True
-
-def file_name_get(first_name_param_str, cmd_info):
-    first_name_param_str = str(first_name_param_str)
-
-    cmd_str = first_name_param_str[:first_name_param_str.find('=')]
-    if cmd_str != '-of':
-        print('the first parameter is -of=')
-        return False
-
-    file_name_str = first_name_param_str[first_name_param_str.find('=') + 1:]
-    cmd_info['file_name'] = file_name_str
-    return True
-    
-def param_nums_check(param_nums):
-    if param_nums == CMD_HELP_PARAM_NUMS:
-        return CMD.CMD_HELP
-    elif param_nums == CMD_EXEC_PARAM_NUMS:
-        return CMD.CMD_EXEC
-    else:
-        return CMD.CMD_INVALID
-    
 if __name__ == '__main__':
-    param_nums = len(sys.argv)
+    args = paras_analysis()
 
-    ret = param_nums_check(param_nums)
-    if ret == CMD.CMD_INVALID:
-        print('please check the number of enter parameters')
-    elif ret == CMD.CMD_EXEC:
-        cmd_info = {'file_name' : '', 'unit' : 0, 'count' : 0}
+    out_file_name = str(args.of)
+    size_unit = str(args.unit)
+    size_count = str(args.count)
 
-        first_name_param_str = sys.argv[1]
-        if file_name_get(first_name_param_str, cmd_info) == True:
-            if file_exists_check(cmd_info['file_name']) == True:
-                print('file already exists')
-                sys.exit(0)
-            else:
-                file_create(cmd_info)
-        else:
-            print('output file name resolution failed')
-            sys.exit(1)
+    if not args_valid_check(out_file_name, size_unit, size_count):
+        sys.exit(-1)
 
-        unit_param_str = sys.argv[2]
-        if unit_get(unit_param_str, cmd_info) == False:
-            print('unit resolution failed')
-            sys.exit(1)
-
-        count_param_str = sys.argv[3]
-        if count_get(count_param_str, cmd_info) == False:
-            print('count resolution failed')
-            sys.exit(1)
-
-        file_write(cmd_info)
+    file_status = out_file_check(out_file_name)
+    if file_status == True:
+        print(f"{out_file_name} already exists")
         sys.exit(0)
-    else:
-        help_param_str = sys.argv[1]
-        help_info_display(help_param_str)
-        sys.exit(0)
+
+    out_file_size = out_file_size(size_unit, size_count)
+
+    out_file_generate(out_file_name, out_file_size)
+
+    sys.exit(0)
